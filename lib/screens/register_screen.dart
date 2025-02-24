ae42bec546b14bdfb9faf2885614a7fb3d1c6249
Nidhi Rijhwani <rijhwaniniddhi@google.com>
2025-02-25 03:49:10 +0530

    Added validation in Registration Form

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,130 +1,284 @@
 import 'package:flutter/material.dart';
+import 'package:socialbee/screens/login_screen.dart';
 
-class RegScreen extends StatelessWidget {
+class RegScreen extends StatefulWidget {
   const RegScreen({Key? key}) : super(key: key);
+
+  @override
+  _RegScreenState createState() => _RegScreenState();
+}
+
+class _RegScreenState extends State<RegScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
+
+  bool _isPasswordVisible = false;
+  bool _isConfirmPasswordVisible = false;
+
+  String? _nameError;
+  String? _emailError;
+  String? _passwordError;
+  String? _confirmPasswordError;
+
+  // Function to validate email
+  String? _validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your email';
+    }
+    final emailRegex = RegExp(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
+    if (!emailRegex.hasMatch(value)) {
+      return 'Please enter a valid email address';
+    }
+    return null;
+  }
+
+  // Function to validate password
+  String? _validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your password';
+    }
+    if (value.length < 6) {
+      return 'Password must be at least 6 characters';
+    }
+    return null;
+  }
+
+  // Function to validate confirm password
+  String? _validateConfirmPassword(String? value) {
+    if (value != _passwordController.text) {
+      return 'Passwords do not match';
+    }
+    return null;
+  }
+
+  // Function to validate name
+  String? _validateName(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your name';
+    }
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(//thanks for watching
-          children: [
-            Container(
-              height: double.infinity,
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(colors: [
-                  Color(0xffB81736),
+      body: Stack(
+        children: [
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFFA53860),
                   Color(0xff281537),
-                ]),
+                ],
               ),
-              child: const Padding(
-                padding: EdgeInsets.only(top: 60.0, left: 22),
-                child: Text(
-                  'Create Your\nAccount',
-                  style: TextStyle(
-                      fontSize: 30,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
+            ),
+            child: const Padding(
+              padding: EdgeInsets.only(top: 60.0, left: 22),
+              child: Text(
+                'Create Your\nAccount',
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 200.0),
-              child: Container(
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(40), topRight: Radius.circular(40)),
-                  color: Colors.white,
-                ),
-                height: double.infinity,
-                width: double.infinity,
-                child:  Padding(
-                  padding: const EdgeInsets.only(left: 18.0,right: 18),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 200.0),
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40), topRight: Radius.circular(40)),
+                color: Colors.white,
+              ),
+              height: double.infinity,
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 18.0, right: 18),
+                child: Form(
+                  key: _formKey,
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const TextField(
+                      // Name input field with validation
+                      TextFormField(
+                        controller: _nameController,
                         decoration: InputDecoration(
-                            suffixIcon: Icon(Icons.check,color: Colors.grey,),
-                            label: Text('Full Name',style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color:Color(0xffB81736),
-                            ),)
-                        ),
-                      ),
-                      const TextField(
-                        decoration: InputDecoration(
-                            suffixIcon: Icon(Icons.check,color: Colors.grey,),
-                            label: Text('Phone or Gmail',style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color:Color(0xffB81736),
-                            ),)
-                        ),
-                      ),
-                      const TextField(
-                        decoration: InputDecoration(
-                            suffixIcon: Icon(Icons.visibility_off,color: Colors.grey,),
-                            label: Text('Password',style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color:Color(0xffB81736),
-                            ),)
-                        ),
-                      ),
-                      const TextField(
-                        decoration: InputDecoration(
-                            suffixIcon: Icon(Icons.visibility_off,color: Colors.grey,),
-                            label: Text('Conform Password',style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color:Color(0xffB81736),
-                            ),)
-                        ),
-                      ),
-
-                      const SizedBox(height: 10,),
-                      const SizedBox(height: 70,),
-                      Container(
-                        height: 55,
-                        width: 300,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          gradient: const LinearGradient(
-                              colors: [
-                                Color(0xffB81736),
-                                Color(0xff281537),
-                              ]
+                          labelText: 'Full Name',
+                          labelStyle: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFFA53860),
+                          ),
+                          suffixIcon: const Icon(
+                            Icons.check,
+                            color: Colors.grey,
                           ),
                         ),
-                        child: const Center(child: Text('SIGN IN',style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.white
-                        ),),),
+                        validator: _validateName,
                       ),
-                      const SizedBox(height: 80,),
+                      const SizedBox(height: 10),
+
+                      // Email input field with validation
+                      TextFormField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          labelText: 'Phone or Gmail',
+                          labelStyle: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFFA53860),
+                          ),
+                          suffixIcon: const Icon(
+                            Icons.check,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                        validator: _validateEmail,
+                      ),
+                      const SizedBox(height: 10),
+
+                      // Password input field with visibility toggle and validation
+                      TextFormField(
+                        controller: _passwordController,
+                        obscureText: !_isPasswordVisible,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          labelStyle: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFFA53860),
+                          ),
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              _isPasswordVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              color: Colors.grey,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _isPasswordVisible = !_isPasswordVisible;
+                              });
+                            },
+                          ),
+                        ),
+                        validator: _validatePassword,
+                      ),
+                      const SizedBox(height: 10),
+
+                      // Confirm Password input field with visibility toggle and validation
+                      TextFormField(
+                        controller: _confirmPasswordController,
+                        obscureText: !_isConfirmPasswordVisible,
+                        decoration: InputDecoration(
+                          labelText: 'Confirm Password',
+                          labelStyle: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFFA53860),
+                          ),
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              _isConfirmPasswordVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              color: Colors.grey,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _isConfirmPasswordVisible = !_isConfirmPasswordVisible;
+                              });
+                            },
+                          ),
+                        ),
+                        validator: _validateConfirmPassword,
+                      ),
+                      const SizedBox(height: 10),
+
+                      const SizedBox(height: 70),
+
+                      // Sign Up button
+                      GestureDetector(
+                        onTap: () {
+                          if (_formKey.currentState?.validate() ?? false) {
+                            // Form is valid, proceed with registration
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('Signing up...')),
+                            );
+                            // Navigate to next screen, or save user data
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const LoginScreen(),
+                              ),
+                            );
+                          } else {
+                            // If the form is not valid, show error message
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('Please fix errors')),
+                            );
+                          }
+                        },
+                        child: Container(
+                          height: 55,
+                          width: 300,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            gradient: const LinearGradient(
+                              colors: [
+                                Color(0xFFA53860),
+                                Color(0xff281537),
+                              ],
+                            ),
+                          ),
+                          child: const Center(
+                            child: Text(
+                              'SIGN UP',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 80),
+
+                      // Sign In redirect text
                       const Align(
                         alignment: Alignment.bottomRight,
                         child: Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
                           crossAxisAlignment: CrossAxisAlignment.end,
                           children: [
-                            Text("Don't have account?",style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.grey
-                            ),),
-                            Text("Sign up",style: TextStyle(///done login page
-                                fontWeight: FontWeight.bold,
-                                fontSize: 17,
-                                color: Colors.black
-                            ),),
+                            Text(
+                              "Already have an account?",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, color: Colors.grey),
+                            ),
+                            Text(
+                              "Sign In",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 17,
+                                  color: Colors.black),
+                            ),
                           ],
                         ),
-                      )
+                      ),
                     ],
                   ),
                 ),
               ),
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
