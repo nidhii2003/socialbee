cbe58e67ddf20588d4d4acbbea31e80f55be2934
Nidhi Rijhwani <rijhwaniniddhi@google.com>
2025-02-25 03:54:09 +0530

    Created app_colors.dart to handle the common colors

---
+++ lib/app_colors.dart
@@ -0,0 +1,8 @@
+// lib/app_colors.dart
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFFA4133C); // Dark Pink
+  static const Color secondaryColor = Color(0xFFFF758F); // Light Pink
+  static const Color accentColor = Color(0xFFFFFFFF); // White for contrast
+}
