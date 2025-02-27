2fb5403ec926d9804b5c2e43e9676c8de1511e73
Nidhi Rijhwani <rijhwaniniddhi@google.com>
2025-02-27 12:04:59 +0530

    Modified primary color and secondary

--- lib/app_colors.dart
+++ lib/app_colors.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryColor = Color(0xFFA4133C); // Dark Pink
-  static const Color secondaryColor = Color(0xFFFF758F); // Light Pink
+  static const Color primaryColor = Color(0xff281537); // Dark Pink
+  static const Color secondaryColor = Color(0xFFA53860); // Light Pink
   static const Color accentColor = Color(0xFFFFFFFF); // White for contrast
 }
